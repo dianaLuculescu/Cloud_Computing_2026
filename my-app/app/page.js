f6e7373eed21ab4aae8186de38d5e8135d328af2
Luculescu Diana-Andreea <luculescudiana@gmail.com>
2026-05-10 02:31:03 +0300

    final push

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Această funcție va trimite utilizatorul direct la /login 
-  // înainte ca pagina de home să fie randată.
+
   redirect("/login");
 }
